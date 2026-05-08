e283df380b4cceff44a776faf1fc1025f50609c7
Leonardo Miranda Amaral <leonardo.miranda@suse.com>
2026-05-08 16:31:20 -0300

    implement Workflow.Submit with phase guard and version validation

--- pkg/publish/workflow.go
+++ pkg/publish/workflow.go
@@ -2,11 +2,23 @@ package publish
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
+	"regexp"
 
+	aifv1 "github.com/SUSE/aif/api/v1alpha1"
 	"github.com/SUSE/aif/pkg/blueprint"
 	"github.com/SUSE/aif/pkg/bundle"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var semverPattern = regexp.MustCompile(`^\d+\.\d+\.\d+$`)
+
+func isValidVersion(v string) bool {
+	return semverPattern.MatchString(v)
+}
 
 // Deps groups the constructor dependencies.
 type Deps struct {
@@ -24,8 +36,51 @@ type workflowImpl struct {
 	deps Deps
 }
 
-func (w *workflowImpl) Submit(_ context.Context, _, _ string, _ SubmitRequest) (bundle.Bundle, error) {
-	return bundle.Bundle{}, ErrNotImplemented
+func (w *workflowImpl) Submit(ctx context.Context, namespace, name string, req SubmitRequest) (bundle.Bundle, error) {
+	if req.User == "" {
+		return bundle.Bundle{}, errors.New("submit: user is required")
+	}
+	if !isValidVersion(req.ProposedVersion) {
+		return bundle.Bundle{}, fmt.Errorf("proposedVersion %q: %w", req.ProposedVersion, ErrInvalidVersion)
+	}
+
+	cr, err := w.deps.Bundles.Get(ctx, namespace, name)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return bundle.Bundle{}, fmt.Errorf("bundle %s/%s: %w", namespace, name, ErrBundleNotFound)
+		}
+		return bundle.Bundle{}, fmt.Errorf("get bundle: %w", err)
+	}
+
+	if cr.Status.Phase != aifv1.BundlePhaseDraft && cr.Status.Phase != aifv1.BundlePhaseChangesRequested {
+		return bundle.Bundle{}, fmt.Errorf(
+			"bundle %s/%s is in phase %q, must be Draft or ChangesRequested: %w",
+			namespace, name, cr.Status.Phase, ErrInvalidTransition,
+		)
+	}
+
+	cr.Status.Phase = aifv1.BundlePhaseSubmitted
+	cr.Status.Submission = &aifv1.SubmissionStatus{
+		ProposedVersion:    req.ProposedVersion,
+		ChangeDescription:  req.ChangeDescription,
+		SubmittedBy:        req.User,
+		SubmittedAt:        metav1.Now(),
+		GenerationAtSubmit: cr.Generation,
+	}
+	cr.Status.Review = nil
+
+	if err := w.deps.Bundles.UpdateStatus(ctx, cr); err != nil {
+		return bundle.Bundle{}, fmt.Errorf("update bundle status: %w", err)
+	}
+
+	w.deps.Logger.Info("bundle submitted",
+		"namespace", namespace,
+		"name", name,
+		"proposedVersion", req.ProposedVersion,
+		"submittedBy", req.User,
+	)
+
+	return bundle.BundleFromCR(cr), nil
 }
 
 func (w *workflowImpl) Withdraw(_ context.Context, _, _ string, _ string) (bundle.Bundle, error) {
