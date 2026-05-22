7b657c5d2c9f9deb9da816b042e1518b1c508e5b
Thiago Bertoldi <thiago.bertoldi@suse.com>
2026-05-22 15:42:56 -0300

    P4-3b: tighten BuildBundleCRForTest docstring

--- pkg/fleet/export_test.go
+++ pkg/fleet/export_test.go
@@ -1,5 +1,4 @@
 package fleet
 
 // BuildBundleCRForTest re-exports buildBundleCR for black-box example tests.
-// Internal callers should use buildBundleCR directly.
 var BuildBundleCRForTest = buildBundleCR
